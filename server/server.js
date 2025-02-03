1cc2782a2de5a2ec1554a7baa9df0539cf87b4b1
sahanrandeepa <155757277+sahanrandeepa@users.noreply.github.com>
2025-02-04 00:33:24 +0530

    Solve mongodb connection error

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require ('cors');
+const connectDB = require('./config/database');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect('mongodb://localhost:27017/we-neighbour')
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
 
 
 app.get('/',(req,res) => {
