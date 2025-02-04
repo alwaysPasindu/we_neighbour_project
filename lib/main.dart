ac4b056d158790c5bbb6d22ba02babb0e357970a
alwaysPasindu <always.pasindu@gmail.com>
2025-02-05 00:32:31 +0530

    configured the main.dart file with some modifications 🔥

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'screens/resident_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WE NEIGHBOUR',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF2E88FF),
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Poppins',
+      ),
+      home: const ResidentHomePage(),
+    );
+  }
+}
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "app demo",
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Good Morning, Pasindu!"),
+//           backgroundColor: Colors.amberAccent,
+//         ),
+//       ),
+//     );
+//   }
+// }
