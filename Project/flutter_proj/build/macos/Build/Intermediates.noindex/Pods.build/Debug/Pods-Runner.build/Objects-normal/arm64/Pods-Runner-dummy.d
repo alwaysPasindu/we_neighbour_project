f30d191332d2060fe1caeecd042ece26f1a779f8
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-02-11 08:33:43 +0530

    Initial commits

---
+++ Project/flutter_proj/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/jehanfernando/Desktop/L5\ Sem\ -\ 1/SDGP/We\ Neighbour/We_Neighbour-SDGP/Project/flutter_proj/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/jehanfernando/Desktop/L5\ Sem\ -\ 1/SDGP/We\ Neighbour/We_Neighbour-SDGP/Project/flutter_proj/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
---
+++ Project/flutter_proj/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/Objects-normal/arm64/path_provider_foundation_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/jehanfernando/Desktop/L5\ Sem\ -\ 1/SDGP/We\ Neighbour/We_Neighbour-SDGP/Project/flutter_proj/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/module.modulemap \
+  /Users/jehanfernando/Desktop/L5\ Sem\ -\ 1/SDGP/We\ Neighbour/We_Neighbour-SDGP/Project/flutter_proj/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/DerivedSources/path_provider_foundation_vers.c
