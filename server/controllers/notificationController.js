d0093ab6f86045997f3a582eb2ff49d83f5b2610
sahanrandeepa <155757277+sahanrandeepa@users.noreply.github.com>
2025-02-14 02:14:30 +0530

    feat(notifications):  create management notifications & display

---
+++ server/controllers/notificationController.js
@@ -0,0 +1,31 @@
+const ManagementNotification = require('../models/ManagementNotifications');
+const CommunityNotification = require('../models/CommunityNotification');
+
+//create Management notifications
+exports.createManagementNotification = async(req,res) => {
+    try{
+        const{title,message} = req.body;
+        const notification = new ManagementNotification({
+            title,
+            message,
+            createBy: req.user.id,
+        });
+        await notification.save();
+        res.status(201).json({message:"Management Notification created successfully!"});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message:"Server Error"});
+    }
+};
+
+//Display all management Notifications
+exports.getManagementNotifications = async(req,res) => {
+    try{
+        const notifications = await ManagementNotification.find().sort({createdAt:-1}).populate('createdBy','name');
+        res.json(notifications);
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message:"Server Error"});
+    }
+};
+
