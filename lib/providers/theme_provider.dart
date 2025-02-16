74ef18a2403b2967d0cf8b75dcb2e0b46a9939fe
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-02-16 08:26:33 +0530

    Updated theme provider page

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.dark;
+  ThemeMode _themeMode = ThemeMode.light;
 
   ThemeMode get themeMode => _themeMode;
 
   bool get isDarkMode => _themeMode == ThemeMode.dark;
 
-  void toggleTheme(bool isOn) {
-    _themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+  void toggleTheme() {
+    _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
     notifyListeners();
   }
 }
