6ec9d2df92b98f1764ceaebef735e73496d8b17a
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-02-16 09:57:31 +0530

    Changed accent color in theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class AppTheme {
   // Define primary colors
   static const Color primaryColor = Color(0xFF12284C); // Dark blue
-  static const Color accentColor = Color(0xFF4B7DFF);  // Light blue
+  static const Color accentColor = Color.fromARGB(255, 255, 255, 255);  // Light blue
   static const Color backgroundColor = Colors.white;
 
   // Text styles
