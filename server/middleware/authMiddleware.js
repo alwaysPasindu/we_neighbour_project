8d61dff875f5078712950ce2da5a96d457327e74
sahanrandeepa <155757277+sahanrandeepa@users.noreply.github.com>
2025-02-19 00:40:17 +0530

    fix(notifications): resolve JWT token validation

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ exports.authenticate = function(req, res, next){
     if(!token) return res.status(401).json({message:"No token, authorization failed"});
 
     try{
-        const decoded = jwt.verify(token, process.env.JWT_SCECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
 
         next();
