1b62f9a7a961282f6929b8e57e8852cb3c5818a4
alwaysPasindu <always.pasindu@gmail.com>
2025-02-18 10:15:30 +0530

    initialized the project structure files 🤞

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Git Projects\we_neighbour_project-test2\flutter_application_1"
+export "FLUTTER_APPLICATION_PATH=D:\Git Projects\we_neighbour_project\flutter_application_1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
