159c72e09e56077c9bb20f1ee499615a758a413c
sahanrandeepa <155757277+sahanrandeepa@users.noreply.github.com>
2025-02-27 05:27:55 +0530

    feat(maintenance): add maintenance model

---
+++ server/models/Maintenance.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const MaintenanceSchema = new mongoose.Schema({
+    title:{type:String, required:true},
+    description: {type:String, required:true},
+    resident:{
+        id:mongoose.Schema.Types.ObjectId,
+        ref:'Resident',
+        required:true,
+    },
+    residentName: {type:String, required:true},
+    apartmentcode: {type:String, required:true},
+    status: {type:String, enum:['Pending', 'Done'], default:'Pending'},
+    createdAt: {type:Date, default:Date.now},
+    ratings: [
+        {
+            resident: {type: mongoose.Schema.Types.ObjectId, ref:'Resident'},
+            stars: {type: Number, min:1, max:5},
+        },
+    ],
+});
+
+module.exports = mongoose.model('Maintenance', MaintenanceSchema);
