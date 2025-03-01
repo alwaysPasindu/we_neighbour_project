80e45cc700c5dd07c5a81306758e445d850cf51a
sahanrandeepa <155757277+sahanrandeepa@users.noreply.github.com>
2025-03-01 21:42:54 +0530

    feat(Resource Sharing): add get resource request feature

--- server/controllers/resourceController.js
+++ server/controllers/resourceController.js
@@ -1,8 +1,8 @@
 const Resource = require('../models/Resource');
 const { findById } = require('../models/Service');
 
-
-exports.createRequest = async(req,res) => {
+//create resource request
+exports.createResourceRequest = async(req,res) => {
     try{
         const{resourceName, description, quantity} = req.body;
         const residentId = req.user.id;
@@ -21,6 +21,20 @@ exports.createRequest = async(req,res) => {
         await resource.save();
 
         res.status(201).json({message:"Resource request cereated successfully"});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message:"Server Error"});
+    }
+};
+
+//get resource request
+exports.getResourceRequest = async(req,res) =>{
+    try{
+        const resource = await Resource.find({status:'Active'})
+        .sort({createdAt: -1})
+        .populate('resident','name apartmentCode');
+        
+        res.json(request);
     }catch(error){
         console.error(error);
         res.status(500).json({message:"Server Error"});
