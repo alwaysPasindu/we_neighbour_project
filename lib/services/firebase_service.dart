bd214f9117f527e7dd62b8e6d12f56a192f06474
Chamuditha Nonis <155380000+chamuditha6@users.noreply.github.com>
2025-03-04 10:50:51 +0530

    saving in firebase

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -7,10 +7,23 @@ class FirebaseService {
     await _firestore.collection('events').add({
       'title': title,
       'date': date,
+      'createdAt': FieldValue.serverTimestamp(),
     });
   }
 
   Stream<QuerySnapshot> getEvents() {
     return _firestore.collection('events').orderBy('date').snapshots();
+  }
+
+  Future<void> updateEvent(String docId, String title, DateTime date) async {
+    await _firestore.collection('events').doc(docId).update({
+      'title': title,
+      'date': date,
+      'updatedAt': FieldValue.serverTimestamp(),
+    });
+  }
+
+  Future<void> deleteEvent(String docId) async {
+    await _firestore.collection('events').doc(docId).delete();
   }
 }
