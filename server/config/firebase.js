3dd6557866d7c234ae32f1b19cdf62911e62fa85
sahanrandeepa <155757277+sahanrandeepa@users.noreply.github.com>
2025-03-22 23:21:12 +0530

    refactor(auth): change firebase.js

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,13 +1,20 @@
-// config/firebase.js
-const admin = require('firebase-admin');
 require('dotenv').config();
+const admin = require('firebase-admin');
+//const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-const serviceAccount = require('../serviceAccountKey.json'); // Path to your service account key
-
+const serviceAccount = require('../serviceAccountKey.json');
+// Decode the Base64-encoded JSON
+/*
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf8')
+);
+*/
+// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
 
-module.exports = { db };
+console.log("Firebase Admin initialized successfully!");
+module.exports = { admin, db }; 
