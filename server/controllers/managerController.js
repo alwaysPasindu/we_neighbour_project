b79c1d24b4600779caa124c0b4d042e795dfa805
sahanrandeepa <155757277+sahanrandeepa@users.noreply.github.com>
2025-03-22 23:26:46 +0530

    refactor(auth):change managerController

--- server/controllers/managerController.js
+++ server/controllers/managerController.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcrypt');
 const {connectDB,centralDB} = require('../config/database');
 const ManagerSchema = require('../models/Manager'); 
 const ApartmentSchema = require('../models/Apartment');
-const { syncUserToFirebase } = require('../utils/firebaseSync');
 
 // Central Manager model (for storing all managers' info)
 const CentralManager = centralDB.model('CentralManager', ManagerSchema);
@@ -12,7 +11,9 @@ exports.registerManager = async (req, res) => {
     try {
         const { name, nic, email, password, phone, address, apartmentName } = req.body;
 
-        const Apartment = centralDB.model('Apartment', ApartmentSchema);
+        const centralDbConnection = centralDB;
+        const Apartment = centralDbConnection.model('Apartment', ApartmentSchema);
+
         const existingApartment = await Apartment.findOne({ apartmentName });
         if (existingApartment) {
             return res.status(400).json({ message: 'Apartment already exists' });
@@ -28,14 +29,14 @@ exports.registerManager = async (req, res) => {
             return res.status(400).json({ message: 'Manager already exists' });
         }
 
-        //const db = await connectDB(apartmentName);
+        const db = await connectDB(apartmentName);
 
-        //const Manager = db.model('Manager', ManagerSchema);
+        const Manager = db.model('Manager', ManagerSchema);
 
         // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        /* Save the new manager
+        // Save the new manager
         const newManager = new Manager({
             name,
             nic,
@@ -45,7 +46,7 @@ exports.registerManager = async (req, res) => {
             address,
             apartmentName,
         });
-        await newManager.save();*/
+        await newManager.save();
 
         const centralManager = new CentralManager({
             name,
@@ -59,8 +60,8 @@ exports.registerManager = async (req, res) => {
         await centralManager.save();
 
 
-        
-        return res.status(201).json({ message: 'Manager registration request submitted. Awaiting admin approval' });
+        console.log(`Manager registered successfully in database: ${apartmentName}`);
+        return res.status(201).json({ message: 'Manager registered successfully!' });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Server error' });
